c2c0904e91c7cf96e1b8d3c172d4be6a078640cf
pavantanni <pavantanni45@gmail.com>
2026-04-17 21:30:19 +0530

    Fixed baseURL: added /api

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API = axios.create({
-  baseURL: "https://mern-vr-backend.onrender.com/",
+  baseURL: "https://onrender.com",
 });
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
@@ -8,10 +8,6 @@ API.interceptors.request.use((req) => {
     req.headers.Authorization = `Bearer ${token}`;
   }
   return req;
-});
-
-const API = axios.create({
-  baseURL: "https://onrender.com",
 });
 
 export default API;
