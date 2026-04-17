da8932c266637bfeafcea5ca475ceebd612c8c4b
pavantanni <pavantanni45@gmail.com>
2026-04-17 16:29:47 +0530

    Update frontend API URL to production Render link

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,14 +1,13 @@
-import axios from 'axios'
+import axios from "axios";
 const API = axios.create({
-    baseURL:"http://localhost:5000/api"
-})
-API.interceptors.request.use((req)=>{
-    const token=localStorage.getItem("token")
-    if(token){
-        req.headers.Authorization=`Bearer ${token}`
-    }
-    return req
-})
+  baseURL: "https://mern-vr-backend.onrender.com/",
+});
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+  return req;
+});
 
-
-export default API
+export default API;
